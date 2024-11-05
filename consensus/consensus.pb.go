55d0dce7e7c2ae74802347fb8237b202aa658df0
Jogge <61437971+0xjogge@users.noreply.github.com>
2024-11-05 10:40:04 +0100

    Fix error in proto file

--- consensus/consensus.pb.go
+++ consensus/consensus.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.35.1
 // 	protoc        v5.28.2
-// source: consensus.proto
+// source: consensus/consensus.proto
 
 package consensus
 
@@ -30,7 +30,7 @@ type Request struct {
 
 func (x *Request) Reset() {
 	*x = Request{}
-	mi := &file_consensus_proto_msgTypes[0]
+	mi := &file_consensus_consensus_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -42,7 +42,7 @@ func (x *Request) String() string {
 func (*Request) ProtoMessage() {}
 
 func (x *Request) ProtoReflect() protoreflect.Message {
-	mi := &file_consensus_proto_msgTypes[0]
+	mi := &file_consensus_consensus_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -55,7 +55,7 @@ func (x *Request) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Request.ProtoReflect.Descriptor instead.
 func (*Request) Descriptor() ([]byte, []int) {
-	return file_consensus_proto_rawDescGZIP(), []int{0}
+	return file_consensus_consensus_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Request) GetTimestamp() int32 {
@@ -70,13 +70,13 @@ type Reply struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Access    bool  `protobuf:"varint,2,opt,name=access,proto3" json:"access,omitempty"`
-	Timestamp int32 `protobuf:"varint,3,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	Access    bool  `protobuf:"varint,1,opt,name=access,proto3" json:"access,omitempty"`
+	Timestamp int32 `protobuf:"varint,2,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
 }
 
 func (x *Reply) Reset() {
 	*x = Reply{}
-	mi := &file_consensus_proto_msgTypes[1]
+	mi := &file_consensus_consensus_proto_msgTypes[1]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -88,7 +88,7 @@ func (x *Reply) String() string {
 func (*Reply) ProtoMessage() {}
 
 func (x *Reply) ProtoReflect() protoreflect.Message {
-	mi := &file_consensus_proto_msgTypes[1]
+	mi := &file_consensus_consensus_proto_msgTypes[1]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -101,7 +101,7 @@ func (x *Reply) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Reply.ProtoReflect.Descriptor instead.
 func (*Reply) Descriptor() ([]byte, []int) {
-	return file_consensus_proto_rawDescGZIP(), []int{1}
+	return file_consensus_consensus_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *Reply) GetAccess() bool {
@@ -118,41 +118,42 @@ func (x *Reply) GetTimestamp() int32 {
 	return 0
 }
 
-var File_consensus_proto protoreflect.FileDescriptor
+var File_consensus_consensus_proto protoreflect.FileDescriptor
 
-var file_consensus_proto_rawDesc = []byte{
-	0x0a, 0x0f, 0x63, 0x6f, 0x6e, 0x73, 0x65, 0x6e, 0x73, 0x75, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x22, 0x27, 0x0a, 0x07, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x1c, 0x0a, 0x09,
-	0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52,
-	0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x22, 0x3d, 0x0a, 0x05, 0x52, 0x65,
-	0x70, 0x6c, 0x79, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x18, 0x02, 0x20,
-	0x01, 0x28, 0x08, 0x52, 0x06, 0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x12, 0x1c, 0x0a, 0x09, 0x74,
-	0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x03, 0x20, 0x01, 0x28, 0x05, 0x52, 0x09,
-	0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x32, 0x2e, 0x0a, 0x09, 0x43, 0x6f, 0x6e,
-	0x73, 0x65, 0x6e, 0x73, 0x75, 0x73, 0x12, 0x21, 0x0a, 0x0d, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
-	0x74, 0x41, 0x63, 0x63, 0x65, 0x73, 0x73, 0x12, 0x08, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
-	0x74, 0x1a, 0x06, 0x2e, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x42, 0x0d, 0x5a, 0x0b, 0x2e, 0x2f, 0x63,
-	0x6f, 0x6e, 0x73, 0x65, 0x6e, 0x73, 0x75, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_consensus_consensus_proto_rawDesc = []byte{
+	0x0a, 0x19, 0x63, 0x6f, 0x6e, 0x73, 0x65, 0x6e, 0x73, 0x75, 0x73, 0x2f, 0x63, 0x6f, 0x6e, 0x73,
+	0x65, 0x6e, 0x73, 0x75, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x27, 0x0a, 0x07, 0x52,
+	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x1c, 0x0a, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74,
+	0x61, 0x6d, 0x70, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73,
+	0x74, 0x61, 0x6d, 0x70, 0x22, 0x3d, 0x0a, 0x05, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x12, 0x16, 0x0a,
+	0x06, 0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x61,
+	0x63, 0x63, 0x65, 0x73, 0x73, 0x12, 0x1c, 0x0a, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61,
+	0x6d, 0x70, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74,
+	0x61, 0x6d, 0x70, 0x32, 0x2e, 0x0a, 0x09, 0x43, 0x6f, 0x6e, 0x73, 0x65, 0x6e, 0x73, 0x75, 0x73,
+	0x12, 0x21, 0x0a, 0x0d, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x41, 0x63, 0x63, 0x65, 0x73,
+	0x73, 0x12, 0x08, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x06, 0x2e, 0x52, 0x65,
+	0x70, 0x6c, 0x79, 0x42, 0x0d, 0x5a, 0x0b, 0x2e, 0x2f, 0x63, 0x6f, 0x6e, 0x73, 0x65, 0x6e, 0x73,
+	0x75, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_consensus_proto_rawDescOnce sync.Once
-	file_consensus_proto_rawDescData = file_consensus_proto_rawDesc
+	file_consensus_consensus_proto_rawDescOnce sync.Once
+	file_consensus_consensus_proto_rawDescData = file_consensus_consensus_proto_rawDesc
 )
 
-func file_consensus_proto_rawDescGZIP() []byte {
-	file_consensus_proto_rawDescOnce.Do(func() {
-		file_consensus_proto_rawDescData = protoimpl.X.CompressGZIP(file_consensus_proto_rawDescData)
+func file_consensus_consensus_proto_rawDescGZIP() []byte {
+	file_consensus_consensus_proto_rawDescOnce.Do(func() {
+		file_consensus_consensus_proto_rawDescData = protoimpl.X.CompressGZIP(file_consensus_consensus_proto_rawDescData)
 	})
-	return file_consensus_proto_rawDescData
+	return file_consensus_consensus_proto_rawDescData
 }
 
-var file_consensus_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_consensus_proto_goTypes = []any{
+var file_consensus_consensus_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_consensus_consensus_proto_goTypes = []any{
 	(*Request)(nil), // 0: Request
 	(*Reply)(nil),   // 1: Reply
 }
-var file_consensus_proto_depIdxs = []int32{
+var file_consensus_consensus_proto_depIdxs = []int32{
 	0, // 0: Consensus.RequestAccess:input_type -> Request
 	1, // 1: Consensus.RequestAccess:output_type -> Reply
 	1, // [1:2] is the sub-list for method output_type
@@ -162,27 +163,27 @@ var file_consensus_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_consensus_proto_init() }
-func file_consensus_proto_init() {
-	if File_consensus_proto != nil {
+func init() { file_consensus_consensus_proto_init() }
+func file_consensus_consensus_proto_init() {
+	if File_consensus_consensus_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_consensus_proto_rawDesc,
+			RawDescriptor: file_consensus_consensus_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_consensus_proto_goTypes,
-		DependencyIndexes: file_consensus_proto_depIdxs,
-		MessageInfos:      file_consensus_proto_msgTypes,
+		GoTypes:           file_consensus_consensus_proto_goTypes,
+		DependencyIndexes: file_consensus_consensus_proto_depIdxs,
+		MessageInfos:      file_consensus_consensus_proto_msgTypes,
 	}.Build()
-	File_consensus_proto = out.File
-	file_consensus_proto_rawDesc = nil
-	file_consensus_proto_goTypes = nil
-	file_consensus_proto_depIdxs = nil
+	File_consensus_consensus_proto = out.File
+	file_consensus_consensus_proto_rawDesc = nil
+	file_consensus_consensus_proto_goTypes = nil
+	file_consensus_consensus_proto_depIdxs = nil
 }
--- consensus/consensus_grpc.pb.go
+++ consensus/consensus_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.28.2
-// source: consensus.proto
+// source: consensus/consensus.proto
 
 package consensus
 
@@ -117,5 +117,5 @@ var Consensus_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "consensus.proto",
+	Metadata: "consensus/consensus.proto",
 }
