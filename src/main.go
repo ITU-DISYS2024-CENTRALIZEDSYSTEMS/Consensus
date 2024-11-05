535665f5966cc33c98d0cca882776ca8e36cd946
Jogge <61437971+0xjogge@users.noreply.github.com>
2024-11-05 22:30:27 +0100

    Minor code cleanup

--- src/main.go
+++ src/main.go
@@ -1,58 +1,59 @@
 package main
 
 import (
-	consensus "consensus/consensus"
 	"context"
+	"errors"
 	"log"
 	"math/rand/v2"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
+
+	"consensus/consensus"
 
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type Peer struct {
-	name string
-}
-
 type ConsensusServer struct {
 	consensus.UnimplementedConsensusServer
 	timestamp  int32
+	identifier int
 	peers      []string
-	requesting bool
-	mu         sync.Mutex
+	requesting sync.Mutex
+	accessing  sync.Mutex
 }
 
+// Finds the first available port from an array of ports. Then returns an listener on that port.
 func findAvailablePort(ports []string) (listener net.Listener, err error) {
-	for i := 0; i < len(ports); i++ {
-		listener, err := net.Listen("tcp", ":"+ports[i])
-		if err != nil {
-			continue
+	for _, port := range ports {
+		listener, err := net.Listen("tcp", ":"+port)
+		if err == nil {
+			return listener, nil
 		}
-
-		return listener, err
 	}
 
-	return
+	return nil, err
 }
 
-func selectedPortIndex(ports []string, listener net.Listener) int {
+// Returns the index where the current port used by the listener is, in the given array.
+func selectedPortIndex(ports []string, listener net.Listener) (index int, err error) {
 	selectedPort := strconv.Itoa(listener.Addr().(*net.TCPAddr).Port)
 
-	for i := 0; i < len(ports); i++ {
-		if selectedPort == ports[i] {
-			return i
+	for i, port := range ports {
+		if selectedPort == port {
+			return i, nil
 		}
 	}
 
-	return -1
+	return -1, errors.New("port not found")
 }
 
+// Emulates a resource being accessed.
 func useCriticalResource() {
 	log.Printf("I got access the resource!")
 	log.Println("Waiting 5 secounds to ask again...")
@@ -60,36 +61,33 @@ func useCriticalResource() {
 }
 
 func getAccessToResource(server *ConsensusServer) {
-	server.mu.Lock()
-
-	time.Sleep(5 * time.Second)
-
 	grpcOptions := grpc.WithTransportCredentials(insecure.NewCredentials())
 
 	for {
-		accessGranted := false
-		for i := 0; i < len(server.peers); i++ {
-			conn, err := grpc.NewClient(":"+server.peers[i], grpcOptions)
+		accessGranted := true
+		for _, peer := range server.peers {
+			conn, err := grpc.NewClient(":"+peer, grpcOptions)
 			if err != nil {
-				log.Fatalf("Cannot create client: %s", err)
+				log.Fatalf("Failed to connect to peer: %s %s", peer, err)
 			}
+			defer conn.Close()
 
 			client := consensus.NewConsensusClient(conn)
 			ctx := context.Background()
 			reply, err := client.RequestAccess(ctx, &consensus.Request{
-				Timestamp: server.timestamp,
+				Timestamp:  server.timestamp,
+				Identifier: int32(server.identifier),
 			})
-			if err != nil {
-				log.Fatalf("Cannot request access: %s", err)
+			if err != nil || !reply.Access {
+				accessGranted = false
+				break
 			}
-
-			accessGranted = reply.Access
 		}
 
 		if accessGranted {
 			useCriticalResource()
 		} else {
-			log.Println("Did not get access, waiting...")
+			log.Println("Access denied, retrying...")
 			server.timestamp++
 			time.Sleep(time.Duration(rand.IntN(1000)) * time.Millisecond)
 		}
@@ -97,12 +95,12 @@ func getAccessToResource(server *ConsensusServer) {
 }
 
 func (server *ConsensusServer) RequestAccess(context context.Context, request *consensus.Request) (*consensus.Reply, error) {
-	server.mu.Lock()
-	if request.Timestamp <= server.timestamp {
-		replyAccess = false
-	}
+	server.requesting.Lock()
+	defer server.requesting.Unlock()
 
-	return &consensus.Reply{}, nil
+	access := request.Timestamp >= server.timestamp
+
+	return &consensus.Reply{Access: access}, nil
 }
 
 func main() {
@@ -111,17 +109,21 @@ func main() {
 
 	listener, err := findAvailablePort(ports)
 	if err != nil {
-		log.Fatalln("Cannot create listener: ", err)
+		log.Fatalln("Cannot create listener:", err)
 	}
 
 	log.Println("Using port:", listener.Addr().String())
 
-	usedPortIndex := selectedPortIndex(ports, listener)
+	usedPortIndex, err := selectedPortIndex(ports, listener)
+	if err != nil {
+		log.Fatalln("Cannot find used port", err)
+	}
+
 	server := grpc.NewServer()
 	service := &ConsensusServer{
 		timestamp:  0,
+		identifier: os.Getpid(),
 		peers:      append(ports[:usedPortIndex], ports[usedPortIndex+1:]...),
-		requesting: false,
 	}
 	consensus.RegisterConsensusServer(server, service)
 
